e386bd0596090f6eafb637050ee6305e1fe9df13
Shuvra Sourav Das <sourav.officialmail@gmail.com>
2020-06-06 14:26:07 +0600

    create data import project

---
+++ HealthJournalDataImport/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HealthJournalDataImport
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
