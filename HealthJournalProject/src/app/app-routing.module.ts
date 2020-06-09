16e8cd835c0fb1b17cd6b0931624aa6ff6571436
coder1495 <lotherington.sarah@gmail.com>
2020-06-09 16:45:46 -0700

    create page

--- HealthJournalProject/src/app/app-routing.module.ts
+++ HealthJournalProject/src/app/app-routing.module.ts
@@ -30,7 +30,11 @@ const routes: Routes = [
     {
         path: 'profile/:id',
         loadChildren: () => import('./profile/profile.module').then(m => m.ProfilePageModule)
-    }
+    },  {
+    path: 'stats',
+    loadChildren: () => import('./home/stats/stats.module').then( m => m.StatsPageModule)
+  }
+
 ];
 
 @NgModule({
